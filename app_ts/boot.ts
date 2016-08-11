de51b7f503521c8fab997c036bda047b78c57d3e
ProximaHaiz <proximaHaiz@gmail.com>
2016-08-12 01:15:27 +0300

    Update boot.ts

--- app_ts/boot.ts
+++ app_ts/boot.ts
@@ -1,4 +1,4 @@
-///<reference path="node_modules/angular2/typings/browser.d.ts"/>
+
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from "./app.component";
 import {HTTP_PROVIDERS} from "@angular/http";
